7484993692211608d9881e8425841ef2d7bcc7a4
Tristan Darricau <github@nicofuma.fr>
2015-01-21 14:37:43 +0100

    Launch script

--- launch.sh
+++ launch.sh
@@ -9,7 +9,7 @@ echo 'ensipcserveur.imag.fr slots=1 max-slots=1' > hosts.clean
 taktuk -o output='"$line\n"' -o status -o error -o connector -o taktuk -o info -s -f hosts broadcast exec { hostname } | grep -v "Connection failed" >> hosts.clean
 
 # Deploying
-# taktuk -s -f hosts.clean broadcast put [ "$WORKING_DIR" ] [ "$WORKING_DIR" ]
+taktuk -s -f hosts.clean broadcast put [ "$WORKING_DIR" ] [ "$WORKING_DIR" ]
 
 # Execution
 # mpirun --hostfile hosts.clean sys-rep Makefile $1
