b84fe6cd49620ff5a1ccdd5a246ef6cefe1d5139
Tristan Darricau <github@nicofuma.fr>
2015-01-21 14:44:13 +0100

    Launch script

--- launch.sh
+++ launch.sh
@@ -5,11 +5,15 @@ cp -R ./* "$WORKING_DIR"
 cd "$WORKING_DIR"
 
 # Get hostnames list
+echo 'Construct workers list...'
 echo 'ensipcserveur.imag.fr slots=1 max-slots=1' > hosts.clean
-taktuk -o output='"$line\n"' -o status -o error -o connector -o taktuk -o info -s -f hosts broadcast exec { hostname } | grep -v "Connection failed" >> hosts.clean
+taktuk -o output='"$line\n"' -o status -o error -o connector -o taktuk -o info -s -f hosts broadcast exec { hostname } | grep -v "Connection failed" >> hosts_workers.clean
+cat hosts_workers.clean >> hosts.clean
 
 # Deploying
-taktuk -s -f hosts.clean broadcast put [ "$WORKING_DIR" ] [ "$WORKING_DIR" ]
+echo "Deploy job..."
+taktuk -s -f hosts_workers.clean broadcast put [ "$WORKING_DIR" ] [ "$WORKING_DIR" ]
 
 # Execution
+echo 'Execute job...'
 # mpirun --hostfile hosts.clean sys-rep Makefile $1
