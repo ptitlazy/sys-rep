92467461d3ed476bd3a513325e14d0cb567d8d9c
Tristan Darricau <github@nicofuma.fr>
2015-01-21 15:53:56 +0100

    Launch

--- launch.sh
+++ launch.sh
@@ -9,11 +9,12 @@ cp /user/2/darricat/hosts hosts
 # Get hostnames list
 echo 'Construct workers list...'
 echo 'ensipcserveur.imag.fr slots=1 max-slots=1' > hosts.clean
-taktuk -o output='"$line\n"' -o status -o error -o connector -o taktuk -o info -s -f hosts broadcast exec { hostname && rm -rf /tmp/ 2>/dev/null } | grep -v "Connection failed" >> hosts_workers.clean
+taktuk -o output='"$line\n"' -o status -o error -o connector -o taktuk -o info -s -f hosts broadcast exec { hostname } | grep -v "Connection failed" >> hosts_workers.clean
 cat hosts_workers.clean >> hosts.clean
 
 # Deploying
 echo "Deploy job..."
+taktuk -s -f hosts_workers.clean broadcast exec { rm -rf /tmp/ }
 taktuk -s -f hosts_workers.clean broadcast put [ "$WORKING_DIR" ] [ "$WORKING_DIR" ]
 
 # Execution
