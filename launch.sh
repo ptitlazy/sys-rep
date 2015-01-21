2d09d1c9ef5ac3cc49e1a38fb7d7f444046f7c07
Tristan Darricau <github@nicofuma.fr>
2015-01-21 14:30:44 +0100

    Launch script

---
+++ launch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORKING_DIR=$(mktemp -d)
+cp -R ./* "$WORKING_DIR"
+cd "$WORKING_DIR"
+cp
+
+# Get hostnames list
+echo 'ensipcserveur.imag.fr slots=1 max-slots=1' > hosts.clean
+taktuk -o output='"$line\n"' -o status -o error -o connector -o taktuk -o info -s -f hosts broadcast exec { hostname } | grep -v "Connection failed" >> hosts.clean
+
+# Deploying
+taktuk -s -f hosts.clean broadcast put [ "$WORKING_DIR" ] [ "$WORKING_DIR" ]
+
+# Execution
+mpirun --hostfile hosts sys-rep Makefile $1
